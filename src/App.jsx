476f699475b3b56642fc2d4d0acb071bf80fb436
Amit <amitak3107@gmail.com>
2025-08-19 17:07:57 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,64 @@
 import { useState } from 'react'
-import './App.css'
-import Navbar from './Components/Navbar'
-import HeroSection from './Components/HeroSection'
-import InnovationSection from './Components/InnovationSection'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from './Screens/Home'
-import { SignIn } from './Screens/SignIn'
-import { Contact } from './Screens/Contact'
-import Company from './Screens/Company'
 
+import './App.css'
+
+import Navbar from './Components/Navbar'
+
+import HeroSection from './Components/HeroSection'
+
+import InnovationSection from './Components/InnovationSection'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { Home } from './Screens/Home'
+
+import { SignIn } from './Screens/SignIn'
+
+import { Contact } from './Screens/Contact'
+
+import Company from './Screens/Company'
+ 
 import AcceptingPaymentSection from './Screens/AcceptingPaymentSection'
+
 import InvoiceSection from './Screens/InvoiceSection'
+
 import VirtualAccountSection from './Components/VirtualAccountSection'
+
 import WhiteLabelSection from './Screens/WhiteLabelSection'
 import SignUp from './Screens/SignUp'
-
+ 
+ 
 function App() {
+
   const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-
+<>
+ 
 <BrowserRouter basename='/Kuber-Web'>
 <Routes>
 <Route path='/' element={<Home/>} />
 <Route path='/login' element={<SignIn/>} />
+<Route path= '/signUp' element={<SignUp/>} />
+<Route path='/contact' element={<Contact/>} />
+<Route path="/company" element={<Company/>} />
+ 
 <Route path="/payment-gateway" element={<AcceptingPaymentSection />} />
 <Route path="/invoice" element={<InvoiceSection />} />
 <Route path="/virtual-account" element={<VirtualAccountSection/>}/>
 <Route path="/whitelabel" element={<WhiteLabelSection />} />
-
+ 
 </Routes>
-
-
-
+ 
+ 
 </BrowserRouter>
-
-
-
+ 
+ 
     </>
-  )
-}
 
+  )
+
+}
+ 
 export default App
+ 
